c27a96cbb372d46823800591294125a5d0db007d
Middle Author Bioinformatics <dna@midauthorbio.com>
2025-09-15 13:21:07 -0400

    Update gb2faa.py

--- gb2faa.py
+++ gb2faa.py
@@ -3,7 +3,7 @@ import argparse
 from Bio import SeqIO
 
 
-def extract_proteins_or_contigs(genbank_file, output_fasta, output_type):
+def extract_proteins_or_contigs(genbank_file, output_fasta, output_type, protein_only=False):
     has_proteins = False
     records = list(SeqIO.parse(genbank_file, "genbank"))
     out = open(output_type, "w")
@@ -26,23 +26,27 @@ def extract_proteins_or_contigs(genbank_file, output_fasta, output_type):
                     if feature.type == "CDS" and "translation" in feature.qualifiers:
                         counter += 1
                         locus_tag = feature.qualifiers.get("locus_tag", ["unknown"])[0]
-                        # product = feature.qualifiers.get("product", ["unknown"])[0]
                         protein_seq = feature.qualifiers["translation"][0]
-                        # fasta_out.write(f">{record.name};{locus_tag};{product};{str(counter)}\n{protein_seq}\n")
                         fasta_out.write(f">{record.name};{locus_tag};{counter}\n{protein_seq}\n")
         else:
-            out.write("contigs")
-            for record in records:
-                fasta_out.write(f">{record.name}\n{record.seq}\n")
-        out.close()
+            if not protein_only:
+                out.write("contigs")
+                for record in records:
+                    fasta_out.write(f">{record.name}\n{record.seq}\n")
+            else:
+                out.write("no_proteins_found")
+    out.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Extract protein sequences from a GenBank file and save them in FASTA format. If no proteins are found, output contigs instead.")
+        description="Extract protein sequences from a GenBank file and save them in FASTA format. "
+                    "If no proteins are found, output contigs instead (unless --protein-only is set).")
     parser.add_argument("input", help="Path to the input GenBank file")
     parser.add_argument("output", help="Path to the output FASTA file")
     parser.add_argument("outputType", help="Path to the output type file")
+    parser.add_argument("--protein-only", action="store_true",
+                        help="Only write proteins. If no proteins are found, no contigs will be written.")
 
     args = parser.parse_args()
-    extract_proteins_or_contigs(args.input, args.output, args.outputType)
+    extract_proteins_or_contigs(args.input, args.output, args.outputType, protein_only=args.protein_only)
