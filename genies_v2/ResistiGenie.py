600cab6a3d7a291d31760478190aeead038f6840
Middle Author Bioinformatics <dna@midauthorbio.com>
2026-06-16 13:32:32 -0400

    input

--- genies_v2/ResistiGenie.py
+++ genies_v2/ResistiGenie.py
@@ -342,7 +342,8 @@ def main():
     try:
         os.listdir(args.out)
         print("Looks like you already have a directory with the name: " + args.out)
-        answer = input("Would you like ResistiGenie to proceed and potentially overwrite files in this directory? (y/n): ")
+        # answer = input("Would you like ResistiGenie to proceed and potentially overwrite files in this directory? (y/n): ")
+        answer = "y"
         if answer == "y":
             print("Ok, proceeding with analysis!")
             try:
--- genies_v2/SporeGenie.py
+++ genies_v2/SporeGenie.py
@@ -335,7 +335,8 @@ def main():
     try:
         os.listdir(args.out)
         print("Looks like you already have a directory with the name: " + args.out)
-        answer = input("Would you like SporeGenie to proceed and potentially overwrite files in this directory? (y/n): ")
+        # answer = input("Would you like SporeGenie to proceed and potentially overwrite files in this directory? (y/n): ")
+        input = "y"
         if answer == "y":
             print("Ok, proceeding with analysis!")
             try:
